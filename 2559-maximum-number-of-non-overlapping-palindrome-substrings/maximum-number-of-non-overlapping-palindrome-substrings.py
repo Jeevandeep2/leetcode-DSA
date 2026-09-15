82e8e3c0b8671969e06a9af8e94bb481ede7d279
Jeevandeep K <jeevandeepk260@gmail.com>
2026-09-15 14:48:12 +0530

    Time: 3 ms (95.88%) | Memory: 19.3 MB (72.68%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.py
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.py
@@ -3,7 +3,6 @@ class Solution:
         n = len(s)
         ans = 0
         i = 0
-        
         while i < n:
             if i + k <= n and s[i:i+k] == s[i:i+k][::-1]:
                 ans += 1
@@ -13,5 +12,4 @@ class Solution:
                 i += k + 1  
             else:
                 i += 1  
-                
         return ans
