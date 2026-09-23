55d41ec5da70e28564e51f02361b3c7079085925
Jeevandeep K <jeevandeepk260@gmail.com>
2026-09-24 00:12:04 +0530

    Time: 60 ms (88.85%) | Memory: 30.7 MB (93.25%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -2,6 +2,7 @@ class Solution:
     def minOperations(self, nums: list[int], x: int) -> int:
         target = sum(nums) - x
         
+        
         if target < 0: 
             return -1
         if target == 0: 
