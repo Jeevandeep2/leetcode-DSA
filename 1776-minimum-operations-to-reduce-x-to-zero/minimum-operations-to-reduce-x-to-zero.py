658358acff515a352882bcbc2d67e5ac10bfa1a2
Jeevandeep K <jeevandeepk260@gmail.com>
2026-09-24 00:13:46 +0530

    Time: 52 ms (97.07%) | Memory: 31.1 MB (54.43%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -12,7 +12,6 @@ class Solution:
         max_len = -1
         n = len(nums)
 
-        
         for right in range(n):
             current_sum += nums[right]
             while current_sum > target:
