0dfd0e2daff8f59288d721fc9b08745569c1adf2
Jeevandeep K <jeevandeepk260@gmail.com>
2026-09-25 21:50:20 +0530

    Time: 3 ms (79.78%) | Memory: 19.5 MB (81.97%) - LeetSync

--- 1188-brace-expansion-ii/brace-expansion-ii.py
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -1,40 +1,90 @@
 class Solution:
     def braceExpansionII(self, expression: str) -> list[str]:
-        def combine(a, b):
-            result = set()
+        op = []  # Operator stack
+        stk = []  # Set stack
 
-            for x in a:
-                for y in b:
-                    result.add(x + y)
+        # Pop the operator at the top of the stack and perform the calculation
+        def ope():
+            l, r = len(stk) - 2, len(stk) - 1
+            if op[-1] == "+":
+                # Union operation
+                stk[l] |= stk[r]
+            else:
+                # Cartesian product operation
+                tmp = set()
+                for left in stk[l]:
+                    for right in stk[r]:
+                        tmp.add(left + right)
+                stk[l] = tmp
+            op.pop()
+            stk.pop()
 
-            return result
+        for i, ch in enumerate(expression):
+            if ch == ",":
+                # Keep popping operators from the top of the stack until the stack is empty or its top is not a multiplication sign
+                while op and op[-1] == "*":
+                    ope()
+                op.append("+")
+            elif ch == "{":
+                # First determine whether a multiplication sign needs to be added, then push { onto the operator stack
+                if i > 0 and (
+                    expression[i - 1] == "}" or expression[i - 1].isalpha()
+                ):
+                    op.append("*")
+                op.append("{")
+            elif ch == "}":
+                # Keep popping operators from the top of the stack until its top is {
+                while op and op[-1] != "{":
+                    ope()
+                op.pop()
+            else:
+                # First determine whether a multiplication sign needs to be added, then push the newly constructed set onto the set stack
+                if i > 0 and (
+                    expression[i - 1] == "}" or expression[i - 1].isalpha()
+                ):
+                    op.append("*")
+                stk.append({ch})
 
-        def solve(i):
-            result = set()
-            current = {""}
+        while op:
+            ope()
 
-            while i < len(expression) and expression[i] != '}':
+        return sorted(stk[-1])
 
-                if expression[i] == ',':
-                    result.update(current)
-                    current = {""}
-                    i += 1
+        # def combine(a, b):
+        #     result = set()
 
-                elif expression[i] == '{':
-                    inside, i = solve(i + 1)
+        #     for x in a:
+        #         for y in b:
+        #             result.add(x + y)
 
-                    current = combine(current, inside)
+        #     return result
 
-                    i += 1
+        # def solve(i):
+        #     result = set()
+        #     current = {""}
 
-                else:
-                    current = combine(current, {expression[i]})
-                    i += 1
+        #     while i < len(expression) and expression[i] != '}':
 
-            result.update(current)
+        #         if expression[i] == ',':
+        #             result.update(current)
+        #             current = {""}
+        #             i += 1
 
-            return result, i
+        #         elif expression[i] == '{':
+        #             inside, i = solve(i + 1)
 
-        result, _ = solve(0)
+        #             current = combine(current, inside)
 
-        return sorted(result)
+        #             i += 1
+
+        #         else:
+        #             current = combine(current, {expression[i]})
+        #             i += 1
+
+        #     result.update(current)
+
+        #     return result, i
+
+        # result, _ = solve(0)
+
+        # return sorted(result)
