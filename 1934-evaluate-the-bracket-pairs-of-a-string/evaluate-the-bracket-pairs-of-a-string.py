02c0d58b450a383103173f97d9c8d3ccdc6e1f9f
Jeevandeep K <jeevandeepk260@gmail.com>
2026-09-26 19:15:46 +0530

    Time: 31 ms (97.82%) | Memory: 52 MB (21.81%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -2,6 +2,7 @@ class Solution:
     def evaluate(self, s: str, K: List[List[str]]) -> str:
         d = dict(K)
         res, i = [], 0
+        
         while i < len(s):
             if s[i] == '(':
                 j = s.find(')', i + 1)
